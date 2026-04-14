c99a653548d0a2fea0e9b1114abbfeebf1123a91
lucas <lucas.aguilar.vasquez@gmail.com>
2026-04-14 11:43:57 -0400

    primer commit

---
+++ new.py
@@ -0,0 +1,11 @@
+# #pedir dos numeros y mostrar la suma y resta de ambos
+# num1=10
+# num2=6
+# #suma
+# print("suma" , num1+num2)
+# #resta
+# print("resta" , num1-num2)
+
+#otra forma
+n1=int(input("ingrese un numero:"))
+n2=int(input("ingrese un numero:"))
---
+++ prueba.py
@@ -0,0 +1,5 @@
+print("ingrese su nombre")
+nombre=input()
+edad=int(input("ingrese su edad: "))
+
+print("hola", nombre, "su edad en 5 años sera", edad+5)
