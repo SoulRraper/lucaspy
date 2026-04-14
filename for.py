eff2a47cdf240da5c7257c6c6333538844f41292
lucas <lucas.aguilar.vasquez@gmail.com>
2026-04-14 12:48:59 -0400

    ejemplo de for

---
+++ for.py
@@ -0,0 +1,21 @@
+#presentacion y ejemplo de for
+#ejemplo simple
+# for i in rage(3):
+#     print("repeticion N", i+1)
+
+# repeticiones dadas por el usuario
+# pida al usuario un numero
+# y muestre un saludo esa cant
+# de veces
+
+# num=int(input("ingrese un num: "))
+# for i in range(num):
+#     print(i+1, "hola usuario")
+
+# pida al usuario un numero
+# muestre la tabla de multiplicar
+# de ese numero hasta el 10
+
+# num=int(input("ingre un num: "))
+# for i in range(10):
+#     print(num , "*" , i+1, "=", num*(i+1))
--- new.py
+++ new.py
@@ -7,5 +7,25 @@
 # print("resta" , num1-num2)
 
 #otra forma
-n1=int(input("ingrese un numero:"))
-n2=int(input("ingrese un numero:"))
+# n1=int(input("ingrese un numero:"))
+# n2=int(input("ingrese otro numero:"))
+#print("la suma de los numeros es ", n1+n2)
+#print("la resta de los numeros es ", n1-n2)
+
+
+#calcular el iva para cada compra
+print(
+'''
+1.- Pera 1200
+2.- Manzana 1400
+3.- Piña 2000
+''')
+selec=int(input("seleccione una fruta: "))
+if selec==1:
+    print("el total a apagar es", 1200*1.19)
+elif selec==2:
+    print("el total a apagar es", 1400*1.19)
+elif selec==3:
+    print("el total a apagar es", 2000*1.19)
+else:
+    print("numero no valido")
